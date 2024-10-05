0de6449804dee77289c988a528d8364ca682c0da
sharmaryan <panditaryan.as@gmail.com>
2024-10-05 21:39:21 +0530

    feat : get user

--- dist/index.js
+++ dist/index.js
@@ -11,18 +11,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-function updateUser(username_1, _a) {
-    return __awaiter(this, arguments, void 0, function* (username, { firstname, lastname }) {
-        const response = yield prisma.user.update({
+function getUser(username) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield prisma.user.findUnique({
             where: {
                 username
-            },
-            data: {
-                firstname,
-                lastname
-            },
+            }
         });
         console.log(response);
     });
 }
-updateUser('aryansharma', { firstname: 'sharma', lastname: 'aryan' });
+getUser('aryansharma');
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-interface UpdateParams {
-    firstname: string;
-    lastname: string;
-}
-
-async function updateUser(username: string, {
-    firstname,
-    lastname
-}: UpdateParams) {
-    const response = await prisma.user.update({
+async function getUser(username: string) {
+    const response = await prisma.user.findUnique({
         where: {
             username
-        },
-        data: {
-            firstname,
-            lastname
-        },
+        }
     })
     console.log(response)
 }
 
-updateUser('aryansharma', { firstname: 'sharma', lastname: 'aryan' })
+getUser('aryansharma')
